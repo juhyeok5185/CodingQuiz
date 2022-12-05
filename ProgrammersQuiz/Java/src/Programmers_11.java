e544a9cfde3e65e19883df2d6189349344f71c15
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2022-12-06 02:19:06 +0900

    pro11,12,13 added

---
+++ ProgrammersQuiz/Java/src/Programmers_11.java
@@ -0,0 +1,20 @@
+public class Programmers_11 {
+
+    // 정수 배열 numbers가 매개변수로 주어집니다.
+    // numbers의 원소의 평균값을
+    // return하도록 solution 함수를 완성해주세요.
+
+    public static void main(String[] args) {
+        int[] num = { 1, 4, 5, 6, 7, 8, 9 };
+        Programmers_11 pro = new Programmers_11();
+        pro.solution(num);
+    }
+
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+        return answer;
+    }
+}
---
+++ ProgrammersQuiz/Java/src/Programmers_12.java
@@ -0,0 +1,22 @@
+public class Programmers_12 {
+
+    // 정수 n이 주어질 때, n이하의 짝수를
+    // 모두 더한 값을 return 하도록
+    // solution 함수를 작성해주세요.
+
+    public static void main(String[] args) {
+        Programmers_12 pro = new Programmers_12();
+        pro.solution(30);
+    }
+
+    public int solution(int n) {
+
+        int answer = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
