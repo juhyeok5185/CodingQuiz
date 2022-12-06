cdee1d4b597b783dfdd610573b6ca30c776002a8
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2022-12-06 18:26:37 +0900

    pro15 added

---
+++ ProgrammersQuiz/Java/src/Programmers_15.java
@@ -0,0 +1,17 @@
+public class Programmers_15 {
+
+    // 머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다.
+    // 피자를 나눠먹을 사람의 수 n이 주어질 때,
+    // 모든 사람이 피자를 한 조각 이상 먹기 위해
+    // 필요한 피자의 수를 return 하는 solution 함수를 완성해보세요.
+
+    public static void main(String[] args) {
+        Programmers_15 pro = new Programmers_15();
+        pro.solution(3);
+    }
+
+    public int solution(int n) {
+        int answer = (n % 7 == 0) ? n / 7 : n / 7 + 1;
+        return answer;
+    }
+}
