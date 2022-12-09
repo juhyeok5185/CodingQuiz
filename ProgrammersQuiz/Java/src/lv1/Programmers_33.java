5b0455760a9788162d7475d0b71b0e7fb73f632b
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2022-12-09 13:06:44 +0900

    added

---
+++ ProgrammersQuiz/Java/src/lv1/Programmers_33.java
@@ -0,0 +1,15 @@
+public class Programmers_33 {
+    // 어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다.
+    // 정수 n이 매개변수로 주어질 때, n이 제곱수라면
+    // 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+
+    public static void main(String[] args) {
+        Programmers_33 pro = new Programmers_33();
+        pro.solution(20);
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
