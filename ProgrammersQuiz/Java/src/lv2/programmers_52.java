ca7321d41b2131f78a05a50dd0900b19c293f31b
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2022-12-23 23:50:01 +0900

    added

---
+++ ProgrammersQuiz/Java/src/lv2/programmers_52.java
@@ -0,0 +1,22 @@
+public class programmers_52 {
+    // 정수가 담긴 배열 numbers와 문자열
+    // direction가 매개변수로 주어집니다.
+    // 배열 numbers의 원소를 direction방향으로
+    // 한 칸씩 회전시킨 배열을 return하도록
+    // solution 함수를 완성해주세요.
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 6, 7, 8, 9 };
+        solution(arr, "right");
+    }
+
+    public static int[] solution(int[] numbers, String direction) {
+        int[] answer = {};
+        if (direction.equals("right")) {
+
+        } else if (direction.equals("left")) {
+
+        }
+        return answer;
+    }
+}
