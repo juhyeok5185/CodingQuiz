a639ccb4d3341b515d1a47e343026036f06c63f6
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-01-31 08:18:27 +0900

    added

---
+++ ProgrammersQuiz/JavaScript/main.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3,4,5];
+solution(numbers);
+
+function solution(numbers) {
+    var answer = [];
+    for (let i = 0; i < numbers.lentgh; i++){
+        console.log(numbers[i]);
+    }
+    return answer;
+}
